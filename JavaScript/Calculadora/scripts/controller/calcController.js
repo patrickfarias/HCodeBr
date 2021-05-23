1f3553a6838ca073b38263fd55f08f6d8bd18200
“patrickfarias” <patrickfarias@icloud.com>
2021-05-23 14:14:55 -0300

    C07 - Adicionando o Evento Click

--- JavaScript/Calculadora/scripts/controller/calcController.js
+++ JavaScript/Calculadora/scripts/controller/calcController.js
@@ -8,6 +8,7 @@ class CalcController {
       this._timeEl = document.querySelector("#hora");
       this._currentDate;
       this.initialize();
+      this.initButtonsEnvent();
   }
 
   initialize(){
@@ -33,6 +34,16 @@ class CalcController {
 
   initButtonsEnvent(){
     let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+
+    buttons.forEach((btn, index) => {
+
+      btn.addEventListener('click', e => {
+        //console.log(e);
+        //console.log(btn);
+        console.log(btn.className.baseVal.replace("btn-", ""));        
+      })
+    })
+
   }
   
   get displayTime(){
